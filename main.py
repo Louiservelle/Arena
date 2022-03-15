2aa9f69750e03f4dbfc96b3035abfc251dd97530
Ugo Ameslant <ugo.ameslant@ynov.com>
2022-03-15 10:06:57 +0100

    background

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(1080, 720)
+pygame.display.set_caption("Arena")
+size = 1080, 720
+screen = pygame.display.set_mode(size)
 background = pygame.image.load('asset-background2D/_PNG/1/background.png')
 pygame.display.set_caption("Arena")
 running = True
 
 while running:
-    screen.blit(background())
+
+    screen.blit(background, (0, 0))
+
+    pygame.display.flip()
+    for even in pygame.event.get():
+        if even.Type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+
+
