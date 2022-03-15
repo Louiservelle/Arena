6b4e0633a04b54c87656ddd681321efa46baebac
Ugo Ameslant <ugo.ameslant@ynov.com>
2022-03-15 15:01:26 +0100

    personnage

--- Player.py
+++ Player.py
@@ -7,16 +7,21 @@ class Player(pygame.sprite.Sprite):
         self.health = 100
         self.max_health = 100
         self.attack = 20
-        self.velocity = 10
+        self.velocity = 9
+        self.jumpp = 10
         self.image = pygame.image.load('asset-champ2d/PNG/Knight/knight.png')
         self.image = pygame.transform.scale(self.image, (210, 210))
         self.rect = self.image.get_rect()
-        self.rect.x = 800
+        self.rect.x = 200
         self.rect.y = 490
         self.all_projectile = pygame.sprite.Group()
 
     def move_right(self):
+
         self.rect.x += self.velocity
 
     def move_left(self):
         self.rect.x -= self.velocity
+
+    def jump(self):
+        self.rect.y -= self.jumpp
--- main.py
+++ main.py
@@ -8,21 +8,32 @@ screen = pygame.display.set_mode(size)
 background = pygame.image.load('asset-background2D/_PNG/1/background.png')
 background = pygame.transform.scale(background, (1080, 720))
 pygame.display.set_caption("Arena")
+
 running = True
+player = Player()
+pressed = {}
 while running:
 
     screen.blit(background, (0, 0))
+    screen.blit(player.image, player.rect)
     pygame.display.flip()
-    player = Player()
-
+    pygame.display.flip()
+    if pressed.get(pygame.K_d):
+        player.move_right()
+    if pressed.get(pygame.K_q):
+        player.move_left()
+    if pressed.get(pygame.K_SPACE):
+        player.jump()
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        if keys[pygame.K_d]:
-            Player.move_right(player)
-            print("lala")
+        elif event.type == pygame.KEYDOWN:
+            pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            pressed[event.key] = False
+
 
 
 
