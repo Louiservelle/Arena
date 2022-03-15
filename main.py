31972030f5e444f72e4736f43bda1e2feb8a7067
Louis Servelle <louis.servelle@ynov.com>
2022-03-15 10:23:22 +0100

    size background

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ pygame.display.set_caption("Arena")
 size = 1080, 720
 screen = pygame.display.set_mode(size)
 background = pygame.image.load('asset-background2D/_PNG/1/background.png')
+background = pygame.transform.scale(background, (1080, 720))
 pygame.display.set_caption("Arena")
 running = True
-
 while running:
 
     screen.blit(background, (0, 0))
-
     pygame.display.flip()
-    for even in pygame.event.get():
-        if even.Type == pygame.QUIT:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
             pygame.quit()
 
